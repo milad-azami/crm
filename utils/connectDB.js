1d726c2d2531ac1e293dc80a5e731a842b2701a7
Milad Azami <miladazami120@gmail.com>
2023-02-02 15:55:39 +0330

    Connection to DB

---
+++ utils/connectDB.js
@@ -0,0 +1,9 @@
+import mongoose, { mongo } from "mongoose";
+
+async function connectDB() {
+  if (mongoose.connections[0].readyState) return;
+  await mongoose.connect(process.env.MONGO_URI);
+  console.log("Connected to DB");
+}
+
+export default connectDB;
