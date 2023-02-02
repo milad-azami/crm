0e18dbe14f5a5070bb0542ff8b1b0f70cfadc98a
Milad Azami <miladazami120@gmail.com>
2023-02-02 17:06:53 +0330

    StrictQuery added

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 async function connectDB() {
   if (mongoose.connections[0].readyState) return;
+  mongoose.set("strictQuery", false);
   await mongoose.connect(process.env.MONGO_URI);
   console.log("Connected to DB");
 }
