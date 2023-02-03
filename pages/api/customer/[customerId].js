c78e734657448b204ecad65f828c1dfd7e32d35d
Milad Azami <miladazami120@gmail.com>
2023-02-03 13:48:46 +0330

    GET method developed to fetch a specific customer

---
+++ pages/api/customer/[customerId].js
@@ -0,0 +1,28 @@
+import Customer from "../../../models/Customer";
+import connectDB from "../../../utils/connectDB";
+
+export default async function handler(req, res) {
+  try {
+    await connectDB();
+  } catch (err) {
+    console.log(err.message);
+    res
+      .status(500)
+      .json({ status: "failed", message: "Error in connecting to database" });
+    return;
+  }
+
+  if (req.method === "GET") {
+    const id = req.query.customerId;
+    try {
+      const customer = await Customer.findOne({ _id: id });
+      res.status(200).json({ status: "success", data: customer });
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).json({
+        status: "failed",
+        message: "Error in retrieving data from database",
+      });
+    }
+  }
+}
